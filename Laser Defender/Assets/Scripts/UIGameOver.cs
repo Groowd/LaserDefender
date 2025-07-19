b8c38c66f0e2691de282705bbf1e7e63ae4b0fa2
Mehmet Selek <selekmehmet42@gmail.com>
2025-07-20 01:02:29 +0300

    LaserDefender
    2D Game Remade from https://www.udemy.com/course/unitycourse/?couponCode=KEEPLEARNING

---
+++ Laser Defender/Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
+    private void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "You Scored:\n" + scoreKeeper.GetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
